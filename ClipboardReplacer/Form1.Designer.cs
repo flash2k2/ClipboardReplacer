c307c8e8b494184bdd5d9c7f5d7e8cc698b56fd0
Aleksey Kamenskikh <aleksey.kamenskikh@gmail.com>
2023-02-07 17:42:26 +0400

    Добавьте файлы проекта.

---
+++ ClipboardReplacer/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace ClipboardReplacer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.tbSource = new System.Windows.Forms.TextBox();
+            this.tbDest = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Location = new System.Drawing.Point(224, 9);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(75, 23);
+            this.btnReplace.TabIndex = 3;
+            this.btnReplace.Text = "Replace";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbSource
+            // 
+            this.tbSource.Location = new System.Drawing.Point(12, 9);
+            this.tbSource.Name = "tbSource";
+            this.tbSource.Size = new System.Drawing.Size(100, 20);
+            this.tbSource.TabIndex = 1;
+            // 
+            // tbDest
+            // 
+            this.tbDest.Location = new System.Drawing.Point(118, 9);
+            this.tbDest.Name = "tbDest";
+            this.tbDest.Size = new System.Drawing.Size(100, 20);
+            this.tbDest.TabIndex = 2;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 44);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(328, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 66);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tbDest);
+            this.Controls.Add(this.tbSource);
+            this.Controls.Add(this.btnReplace);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Clipboard replacer";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.TextBox tbSource;
+        private System.Windows.Forms.TextBox tbDest;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ ClipboardReplacer/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ClipboardReplacer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var dataobject = Clipboard.GetDataObject();
+            var formats = dataobject.GetFormats();
+            if (formats.Length == 0)
+            {
+                toolStripStatusLabel1.Text = "Empty clipboard";
+                return;
+            };
+            var data = dataobject.GetData(formats[0]);
+            if (data is MemoryStream)
+            {
+                var array = (data as MemoryStream).ToArray();
+                var source = Encoding.Unicode.GetBytes(tbSource.Text);
+                var dest = Encoding.Unicode.GetBytes(tbDest.Text);
+                int count = 0;
+                var newArray = Replace(array, source, dest, out count);
+                Clipboard.SetData(formats[0], new MemoryStream(newArray));
+                toolStripStatusLabel1.Text = $"Replaced {count} times";
+            }
+            else if (data is String)
+            {
+                var newString = (data as String).Replace(tbSource.Text, tbDest.Text);
+                Clipboard.SetData(formats[0], newString);
+                toolStripStatusLabel1.Text = $"Replaced";
+            }
+            else
+            {
+                toolStripStatusLabel1.Text = "Unknown format";
+            };
+        }
+
+        private static byte[] Replace(byte[] input, byte[] pattern, byte[] replacement, out int count)
+        {
+            count = 0;
+            if (pattern.Length == 0)
+            {
+                return input;
+            }
+
+            List<byte> result = new List<byte>();
+            int i;
+          
+            for (i = 0; i <= input.Length - pattern.Length; i++)
+            {
+                bool foundMatch = true;
+                for (int j = 0; j < pattern.Length; j++)
+                {
+                    if (input[i + j] != pattern[j])
+                    {
+                        foundMatch = false;
+                        break;
+                    }
+                }
+
+                if (foundMatch)
+                {
+                    result.AddRange(replacement);
+                    i += pattern.Length - 1;
+                    count++;
+                }
+                else
+                {
+                    result.Add(input[i]);
+                }
+            }
+
+            for (; i < input.Length; i++)
+            {
+                result.Add(input[i]);
+            }
+
+            return result.ToArray();
+        }
+    }
+
+}
